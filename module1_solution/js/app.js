75e5de7e6d9aa34afafafa23bf9a57269c47e2f5
harshal raut <harshal_raut@intuit.com>
2016-09-18 12:37:02 -0700

    Implemented bonus features

--- module1_solution/js/app.js
+++ module1_solution/js/app.js
@@ -4,18 +4,25 @@
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope', '$filter'];
-function LunchCheckController($scope, $filter) {
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
     $scope.checkIfTooMuch = function () {
         if(!$scope.dishes){
             $scope.message = "Please enter data first";
+            $scope.messageStyle = {color:'red'};
+            $scope.textboxStyle = {border: 'solid 1px red'};
         }else{
             $scope.message = (numberOfDishes($scope.dishes) <= 3) ? "Enjoy!" : "Too much!"
+            $scope.messageStyle = {color:'green'};
+            $scope.textboxStyle = {border: 'solid 1px green'};
         }
     };
 
     function numberOfDishes(dishes){
-        return dishes.split(",").length;
+        return dishes
+            .split(",")
+            .filter(function(s){ return s.length != 0}) // do not count empty items
+            .length;
     }
 }
 })();
